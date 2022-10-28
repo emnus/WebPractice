0ccc85bf4adcba10f808df9930af54fbae6932a3
emiliano <emiliano.berisiartua29@hotmail.com>
2022-10-28 09:15:54 -0300

    added link and folders

---
+++ practica/src/components/pages/Main/Main.js
@@ -0,0 +1,16 @@
+import "./Main.css";
+import React, { useState, useEffect } from "react";
+
+import Loading from "../../Loading/Loading";
+
+const Main = () => {
+    return (
+      <div>
+          
+          <Loading/>
+  
+      </div>
+    )
+  }
+  
+  export default Main
